4169bdd6797fc4f9bb6498b3f4fbea37fef22c8b
Thomas Stjernegaard <tsjeppesen@gbif.org>
2020-12-08 13:16:21 +0100

    closes https://github.com/CatalogueOfLife/portal/issues/90

---
+++ _includes/ga.js
@@ -0,0 +1,10 @@
+var head = document.getElementsByTagName('head')[0];
+var js = document.createElement("script");
+js.type = "text/javascript";
+js.async = true;
+js.src = "https://www.googletagmanager.com/gtag/js?id=G-6MXW7NJY01 ";
+head.appendChild(js);
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-6MXW7NJY01');
