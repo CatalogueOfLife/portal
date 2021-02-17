cc4e2b8381f812cf8b51657eb4b6ea55d60dc8a5
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-02-17 12:27:52 +0100

    send pageviews to google analytics manually

--- _includes/ga.js
+++ _includes/ga.js
@@ -8,3 +8,8 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-6MXW7NJY01');
+gtag('event', 'page_view', {
+    page_title: document.title,
+    page_location: window.location.href,
+    page_path: window.location.pathname
+  })
