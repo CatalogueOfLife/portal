abd762d658600b3f936010e0f8079f732f858044
Markus Döring <m.doering@mac.com>
2022-11-17 13:47:06 +0100

    Fix sitemaps and disallow robots access to legacy pages

--- scripts/update-sitemaps.sh
+++ scripts/update-sitemaps.sh
@@ -6,7 +6,7 @@ echo "Rebuilding sitemap index files"
 rm -f ../sitemaps/*
 
 echo "new sitemap sitemap-$FILECOUNTER.txt"
-echo "/sitemaps/sitemap-$FILECOUNTER.txt.gz" > "../_includes/sitemap-index.txt"
+echo "Sitemap: /sitemaps/sitemap-$FILECOUNTER.txt.gz" > "../_includes/sitemap-index.txt"
 while IFS= read -r id
 do
     let COUNTER++
@@ -16,7 +16,7 @@ do
         let FILECOUNTER++
         let COUNTER=1
         echo "new sitemap sitemap-$FILECOUNTER.txt"
-        echo "/sitemaps/sitemap-$FILECOUNTER.txt.gz" >> "../_includes/sitemap-index.txt"
+        echo "Sitemap: /sitemaps/sitemap-$FILECOUNTER.txt.gz" >> "../_includes/sitemap-index.txt"
     fi
     printf "https://www.catalogueoflife.org/data/taxon/$id\n" >> "../sitemaps/sitemap-$FILECOUNTER.txt"
 done < <(curl -s -H "Accept: text/plain" https://api.checklistbank.org/dataset/3LR/taxon/ids)
