a7ee8d0c77660e32ede012958ad55c4fb9ae51c8
Markus Döring <mdoering@gbif.org>
2025-06-24 14:35:29 +0200

    Allow to pass exact release key to sitemap builds

--- scripts/update-sitemaps.sh
+++ scripts/update-sitemaps.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -k|--key)
+      KEY="$2"
+      shift 2
+      ;;
+    *|-*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+API=https://api.checklistbank.org
+
+if [ -z "$KEY" ]
+then
+  echo "Retrieve latest public release"
+  KEY=$(curl -s "$API/dataset/3LR.json" | jq ".key")
+  echo "  $KEY"
+fi
+
+
+
 FILECOUNTER=1
 COUNTER=0
-echo "Rebuilding sitemap index files"
+echo "Rebuilding sitemap index files from release $KEY"
 rm -f ../sitemaps/*
 
 echo "new sitemap sitemap-$FILECOUNTER.txt"
@@ -19,5 +44,5 @@ do
         echo "Sitemap: https://www.catalogueoflife.org/sitemaps/sitemap-$FILECOUNTER.txt.gz" >> "../_includes/sitemap-index.txt"
     fi
     printf "https://www.catalogueoflife.org/data/taxon/$id\n" >> "../sitemaps/sitemap-$FILECOUNTER.txt"
-done < <(curl -s -H "Accept: text/plain" https://api.checklistbank.org/dataset/3LR/taxon/ids)
+done < <(curl -s -H "Accept: text/plain" $API/dataset/$KEY/taxon/ids)
 gzip "../sitemaps/sitemap-$FILECOUNTER.txt"
